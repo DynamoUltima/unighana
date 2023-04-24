fd21b60225a03f405be8e5bb353e41d1c3b7607c
DynamoUltima <dynamo.joey@gmail.com>
2023-04-24 10:33:33 +0000

    footer updated

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -58,7 +58,7 @@ export default NextAuth({
         console.log(password)
 
         try {
-          const response = await axios.post('api/login', {
+          const response = await axios.post('/api/login', {
             email, password
           })
   
